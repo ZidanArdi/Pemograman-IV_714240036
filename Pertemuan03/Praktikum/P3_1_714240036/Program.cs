759a6730a07ab7af6ae6bfc7e402afb177349acd
Zidan Ardiansyah <panjihadi6@gmail.com>
2025-11-22 21:09:27 +0700

    save dulu

---
+++ Pertemuan03/Praktikum/P3_1_714240036/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1_1234567
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("MENENTUKAN INDEKS PRESTASI MAHASISWA");
+                Console.WriteLine("------------------------------------");
+                Console.Write("Masukkan Nama Mahasiswa: ");
+
+                string nama = Console.ReadLine();
+
+                Console.Write("Masukkan Nilai : ");
+                int nilai = Convert.ToInt16(Console.ReadLine());
+
+                string[] grade = { "A", "B", "C", "D" };
+
+                if (nilai > 85)
+                {
+                    Console.WriteLine("Indeks nilai {0} adalah {1}", nama, grade[0]);
+                }
+                else if (nilai >= 70 && nilai <= 85)
+                {
+                    Console.WriteLine("Indeks nilai {0} adalah {1}", nama, grade[1]);
+                }
+                else if (nilai >= 60 && nilai < 70)
+                {
+                    Console.WriteLine("Indeks nilai {0} adalah {1}", nama, grade[2]);
+                }
+                else
+                {
+                    Console.WriteLine("Indeks nilai {0} adalah {1}", nama, grade[3]);
+                }
+
+                Console.Write("Masukkan Indeks yang ditampilkan : ");
+                char indeks = Convert.ToChar(Console.ReadLine());
+                Console.Write("Indeks prestasi {0} adalah ", nama);
+
+                prestasi(indeks);
+
+                Console.Write("\\nIngin mengulang kembali (Y/T) ? ");
+
+            } while (Console.ReadLine() == "Y");
+        }
+        private static void prestasi(char indeks)
+        {
+            switch (indeks)
+            {
+                case 'A':
+                    Console.WriteLine("sangat baik");
+                    break;
+                case 'B':
+                    Console.WriteLine("baik");
+                    break;
+                case 'C':
+                    Console.WriteLine("cukup");
+                    break;
+                case 'D':
+                    Console.WriteLine("buruk");
+                    break;
+                default:
+                    Console.WriteLine("invalid indeks prestasi");
+                    break;
+            }
+        }
+    }
+}
